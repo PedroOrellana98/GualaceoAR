c324adc1eee4c99d5a41a5e7b87e07beb503642f
Pedro Orellana <porellanaj@est.ups.edu.ec>
2022-07-12 21:58:04 -0500

    Implementacion firebase hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDoXlrKBIxj9_cBYAn_YSPGwXAVusiYSC8",
+    authDomain: "gualaceoar.firebaseapp.com",
+    projectId: "gualaceoar",
+    storageBucket: "gualaceoar.appspot.com",
+    messagingSenderId: "194442947543",
+    appId: "1:194442947543:web:69dd34980ecd1e743a7992"
+  }
 };
